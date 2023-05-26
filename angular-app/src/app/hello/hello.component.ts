9ff45ce7b3cf13ecae0b670f8f71088a6dfbb9a0
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-05-26 22:05:25 +0900

    属性の更新

--- angular-app/src/app/hello/hello.component.ts
+++ angular-app/src/app/hello/hello.component.ts
@@ -14,5 +14,10 @@ export class HelloComponent {
     this.title = 'Hello-app';
     this.message = 'This is My First Component!!';
     this.styleClass = 'red';
+
+    setInterval(() => {
+      this.styleClass = this.styleClass === 'red' ? '' : 'red';
+      console.log(this.styleClass);
+    }, 1000);
   }
 }
