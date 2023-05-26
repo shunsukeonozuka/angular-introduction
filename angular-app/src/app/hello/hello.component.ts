874e0e34f098e98c2caea171fc9bcc0b6c142bea
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-05-26 21:56:38 +0900

    プロパティを更新する

--- angular-app/src/app/hello/hello.component.ts
+++ angular-app/src/app/hello/hello.component.ts
@@ -8,13 +8,19 @@ import { Component } from '@angular/core';
 export class HelloComponent {
   title: string;
   message: string;
+  now: Date;
 
   constructor() {
     this.title = 'Hello-app';
     this.message = 'This is My First Component!!';
+    this.now = new Date();
+
+    setInterval(() => {
+      this.now = new Date();
+    }, 1000);
   }
 
   today() {
-    return new Date().toLocaleString();
+    return this.now.toLocaleString();
   }
 }
