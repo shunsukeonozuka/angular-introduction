7a1c9ba596b8ec3c4469067d26398d039fef4b42
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-05-26 21:43:28 +0900

    {{}}でメソッドを呼び出す

--- angular-app/src/app/hello/hello.component.ts
+++ angular-app/src/app/hello/hello.component.ts
@@ -8,11 +8,13 @@ import { Component } from '@angular/core';
 export class HelloComponent {
   title: string;
   message: string;
-  price: number;
 
   constructor() {
     this.title = 'Hello-app';
     this.message = 'This is My First Component!!';
-    this.price = 123450;
+  }
+
+  today() {
+    return new Date().toLocaleString();
   }
 }
